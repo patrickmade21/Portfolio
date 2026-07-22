43a6f608f400597324c3bf6f1518797ae342dc53
patrickmade21 <patrick.emechebe@gmail.com>
2026-07-22 20:57:57 +0100

    Add GitHub link to footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -30,6 +30,17 @@ export default function Footer() {
             <p className="ml-2 h-7">LinkedIn</p>
           </a>
         </li>
+        <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://github.com/patrickmade21"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">GitHub</p>
+          </a>
+        </li>
 
       </ul>
       <p className="mt-8 text-neutral-600 dark:text-neutral-300">
